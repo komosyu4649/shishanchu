ebf87aba8f7ef6cf6c45c011c38f625b4c471ca6
komosyu4649 <komosyu4649@gmail.com>
2023-05-03 22:22:52 +0900

    add: Contentsを複数のCMSから取得して1つのAPIにまとめる

---
+++ src/pages/api/strapi/getAllContents.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { StrapiContent } from "@/type/strapi";
+
+const ACCOUNTS = [
+    {
+        name: "1337"
+    },
+    {
+        name: "1338"
+    }
+]
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // ACCOUNTS分のサーバーAPIを1つの配列にまとめる
+    try {
+    const accounts:StrapiContent[] = await Promise.all(
+      ACCOUNTS.map(async (account) => {
+        const response = await axios.get(`http://localhost:${account.name}/api/contents?populate=*`);
+        return response.data.data;
+      })
+    );
+
+    // 配列をフラット化する
+    const flattenedAccounts = accounts.flat();
+
+    // createdAtが最新になるように並び替え
+    const sortedAccounts = flattenedAccounts.sort((a, b) => new Date(b.attributes.createdAt).getTime() - new Date(a.attributes.createdAt).getTime());
+
+    res.status(200).json(sortedAccounts);
+    } catch (err: any) {
+        res.status(500).json({ statusCode: 500, message: err.message })
+    }
+}
---
+++ src/type/strapi.ts
@@ -0,0 +1,10 @@
+export type StrapiContent = {
+    id: string;
+    type: string;
+    attributes: {
+        title: string;
+        content: string;
+        createdAt: string;
+        updatedAt: string;
+    };
+};
