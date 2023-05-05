943a679276b1fbcc5cdf9a54d1662f3f7cd24c22
komosyu4649 <komosyu4649@gmail.com>
2023-05-05 10:34:51 +0900

    add: ContentsのAPI取得にAuthorizationをかける

--- src/pages/api/strapi/getAllContents.ts
+++ src/pages/api/strapi/getAllContents.ts
@@ -4,19 +4,26 @@ import { StrapiContent } from "@/type/strapi";
 
 const ACCOUNTS = [
     {
-        name: "1337"
+        name: "1337",
+        jwt: process.env.STRAPI_JWT_STORETEMPLATE
     },
     {
-        name: "1338"
+        name: "1338",
+        jwt: process.env.STRAPI_JWT_STORETEMPLATE2
     }
 ]
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // ACCOUNTS分のサーバーAPIを1つの配列にまとめる
+    // ACCOUNTS分のサーバーAPIを1つの配列にまとめるAuthorization認証を加える
+    
     try {
     const accounts:StrapiContent[] = await Promise.all(
       ACCOUNTS.map(async (account) => {
-        const response = await axios.get(`http://localhost:${account.name}/api/contents?populate=*`);
+        const response = await axios.get(`http://localhost:${account.name}/api/contents?populate=*`, {
+            headers: {
+                Authorization: `Bearer ${account.jwt}`
+            }
+        });
         return response.data.data;
       })
     );
@@ -28,7 +35,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const sortedAccounts = flattenedAccounts.sort((a, b) => new Date(b.attributes.createdAt).getTime() - new Date(a.attributes.createdAt).getTime());
 
     res.status(200).json(sortedAccounts);
-    } catch (err: any) {
+    } 
+    catch (err: any) {
         res.status(500).json({ statusCode: 500, message: err.message })
     }
 }
