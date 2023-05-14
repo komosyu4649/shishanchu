9c1e55a3e8a916ae0cbf40cd7bd93c93d7659986
komosyu4649 <komosyu4649@gmail.com>
2023-05-14 12:44:58 +0900

    typeを修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,32 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { StrapiContent } from '../type/strapi'
+import { StrapiContent, StrapiStaff } from '../type/strapi'
 import Content from '@/components/item/Content'
 
 const inter = Inter({ subsets: ['latin'] })
 
-// /api/strapi/index.tsで作成したapiをgetStaticPropsで取得
+// /api/strapi/getTopStaff.tsで作成したapiをgetStaticPropsで取得
 export const getStaticProps = async () => {
   const contents = await fetch('http://localhost:3000/api/strapi/getTopContents')
   const contentsData = await contents.json()
+  const staffs = await fetch('http://localhost:3000/api/strapi/getTopStaffs')
+  const staffsData = await staffs.json()
+  // console.log(staffsData)
   return {
     props: {
       contentsData,
+      staffsData,
     },
   }
 }
 
-export default function Home({ contentsData }: { contentsData: StrapiContent[] }) {
-  // console.log(1, contentsData)
+export default function Home({
+  contentsData,
+  staffsData,
+}: {
+  contentsData: StrapiContent[]
+  staffsData: StrapiStaff[]
+}) {
   return (
     <main>
       {/* contents */}
@@ -30,8 +39,31 @@ export default function Home({ contentsData }: { contentsData: StrapiContent[] }
           <ul className='grid grid-cols-4 gap-8 justify-center'>
             {contentsData.map((content, index) => (
               <Content key={index} content={content} />
-              // console.log(content),
             ))}
+          </ul>
+        </div>
+      </section>
+      {/* staff */}
+      <section className=''>
+        <h2 className=''>
+          <span className=''>人気のスタッフ</span>
+          <span className=''>Staff</span>
+        </h2>
+        <div className=''>
+          <ul className=''>
+            {/* {staffsData.map((staff, index) => (
+              <li key={index} className=''>
+                <Image
+                  src={`http://localhost:1337${staff.icon.url}`}
+                  width={staff.icon.width}
+                  height={staff.icon.height}
+                  alt='test'
+                  className=''
+                />
+                <p className=''>{staff.username}</p>
+                <p className=''>{staff.biography}</p>
+              </li>
+            ))} */}
           </ul>
         </div>
       </section>
