d51b8262a5b39d6253e5a7d7650886e03856fa09
komosyu4649 <komosyu4649@gmail.com>
2023-05-26 22:08:34 +0900

    トップページにクーポン追加

---
+++ src/lib/strapi/fetchCoupons.ts
@@ -0,0 +1,34 @@
+import { ACCOUNTS } from '@/constants/strapi'
+import { StrapiCoupon } from '@/type/strapi'
+import axios from 'axios'
+
+export async function fetchCoupons(limit: number) {
+  const accounts = await Promise.all(
+    ACCOUNTS.map(async (account) => {
+      const response = await axios.get(`http://localhost:${account.name}/api/coupons`, {
+        headers: {
+          Authorization: `Bearer ${account.jwt}`,
+        },
+      })
+
+      const accountDataWithAccountName = response.data.data.map((coupon: StrapiCoupon) => ({
+        ...coupon,
+        accountName: account.name,
+        storeName: account.store,
+        jwt: account.jwt,
+      }))
+      return accountDataWithAccountName
+    }),
+  )
+
+  const flattenedAccounts = accounts.flat()
+  const sortedAccounts = flattenedAccounts.sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+  )
+
+  if (limit) {
+    return sortedAccounts.slice(0, limit)
+  } else {
+    return sortedAccounts
+  }
+}
---
+++ src/pages/api/strapi/getTopCoupons.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { fetchCoupons } from '@/lib/strapi/fetchCoupons'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const coupons = await fetchCoupons(4)
+    res.status(200).json(coupons)
+  } catch (err: any) {
+    res.status(500).json({ statusCode: 500, message: err.message })
+  }
+}
